2ad20bf0509cc568d494dcce18c2cd939ae49f29
rhkdaud268@gmail.com <rhkdaud268@gmail.com>
2023-04-25 21:10:55 +0900

    send id_token

--- src/components/screens/Login.js
+++ src/components/screens/Login.js
@@ -8,6 +8,7 @@ import {
   ActivityIndicator,
   Keyboard,
   TouchableWithoutFeedback,
+  Image
 } from "react-native";
 
 import { windowWidth, windowHeight } from "../../util/WH";
@@ -26,9 +27,9 @@ import { db } from "../../../firebaseConfig.js";
 import * as Google from "expo-auth-session/providers/google";
 import * as WebBrowser from "expo-web-browser";
 import { GetHash } from "../../util/Functions";
+import logo from "../../../assets/logo1.png";
 
 WebBrowser.maybeCompleteAuthSession();
-//
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -54,6 +55,13 @@ export default function Login() {
     if (response?.type === "success") {
       setIsEnter(true);
       const { id_token } = response.params;
+    
+      fetch("https://f854-2001-e60-87e0-f901-1cf5-308a-dbc1-4558.ngrok-free.app/oauth2/google?id_token="+id_token, {
+      method: "GET",
+      headers: { 
+          }, 
+          })
+
       const credential = GoogleAuthProvider.credential(id_token);
       signInWithCredential(auth, credential);
     }
@@ -136,49 +144,9 @@ export default function Login() {
           <>
             <View style={styles.titleBox}>
               <Text style={styles.title}>Diet</Text>
+              <Image source = {require("../../../assets/logo1.png")}/>
             </View>
             <View style={styles.inputBox}>
-              <Text style={styles.loginText}>ID</Text>
-              <TextInput
-                style={styles.loginInput}
-                placeholder={"아이디"}
-                placeholderTextColor="#9e9e9e"
-                onChangeText={setEmail}
-              />
-              <Text style={styles.loginText}>PassWord</Text>
-              <TextInput
-                style={styles.loginInput}
-                placeholder={"비밀번호"}
-                secureTextEntry={true}
-                placeholderTextColor="#9e9e9e"
-                onChangeText={setPassword}
-              />
-
-              <View style={styles.warningContainer}>
-                {isWarning ? (
-                  <Text style={styles.warningMsg}>
-                    일치하는 회원이 없습니다.
-                  </Text>
-                ) : (
-                  ""
-                )}
-              </View>
-              <TouchableOpacity
-                style={styles.buttonContainer}
-                onPress={() => {
-                  userSignIn();
-                }}
-              >
-                <Text style={styles.buttonText}>Login</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={styles.buttonContainer}
-                onPress={() => {
-                  //navigation.navigate("Signup");
-                }}
-              >
-                <Text style={styles.buttonText}>SignUp</Text>
-              </TouchableOpacity>
               <TouchableOpacity
                 style={[styles.buttonContainer, styles.googleLoginButton]}
                 disabled={!request}
@@ -201,7 +169,7 @@ export default function Login() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#000000",
+    backgroundColor: "#9DD84B",
     alignItems: "center",
     justifyContent: "center",
   },
@@ -219,7 +187,7 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 50,
-    color: "#32AAFF",
+    color: "#FFFFFF",
     fontWeight: "bold",
   },
   inputBox: {
@@ -230,14 +198,14 @@ const styles = StyleSheet.create({
     width: windowWidth - 60,
     height: 50,
     borderWidth: 1,
-    borderColor: "#32AAFF",
+    borderColor: "#FFFFFF",
     borderRadius: 5,
     paddingLeft: 10,
     marginBottom: 15,
     color: "#9e9e9e",
   },
   loginText: {
-    color: "#32AAFF",
+    color: "#FFFFFF",
     width: windowWidth - 60,
     fontSize: 16,
     fontWeight: "bold",
@@ -254,7 +222,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   buttonContainer: {
-    backgroundColor: "#32AAFF",
+    backgroundColor: "#FFFFFF",
     width: windowWidth - 60,
     height: 40,
     borderRadius: 5,
@@ -272,7 +240,7 @@ const styles = StyleSheet.create({
   },
   googleButtonText: {
     fontSize: 16,
-    color: "#32AAFF",
+    color: "#FFFFFF",
   },
 
   backgroundBlack: {
@@ -285,95 +253,3 @@ const styles = StyleSheet.create({
 // .../auth/userinfo.email	기본 Google 계정의 이메일 주소 확인
 // .../auth/userinfo.profile	개인정보(공개로 설정한 개인정보 포함) 보기
 // openid	Google에서 내 개인 정보를 나와 연결
-
-// import React, { useState } from "react";  
-// import { StyleSheet, Text, View, TouchableOpacity } from "react-native";  
-// import { AuthSession } from "expo";  
-// import axios from "axios";
-
-// //  
-// const NV\_APP\_ID = "YOUR\_CLIENT\_ID";  
-// const NV\_APP\_SECRET = "YOUR\_CLIENT\_PW"; 
-// const STATE\_STRING = "YOUR\_SECRET\_STRING";
-
-// const styles = StyleSheet.create({  
-// container: {  
-// flex: 1,  
-// backgroundColor: "#fff",  
-// alignItems: "center",  
-// justifyContent: "center"  
-// }  
-// });
-
-// export default function App() {  
-// //useState(), token/code/user  
-// const \[token, setToken\] = useState();  
-// const \[code, setCode\] = useState();  
-// const \[user, setUser\] = useState();
-
-
-// //authsession
-
-
-// async function handlePressAsync() {  
-// let redirectUrl = AuthSession.getRedirectUrl();  
-// console.log(redirectUrl);  
-// console.log(encodeURIComponent(redirectUrl));
-
-
-// const result = await AuthSession.startAsync({
-//   authUrl: `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NV_APP_ID}&redirect_uri=${encodeURIComponent(
-//     redirectUrl
-//   )}&state=${STATE_STRING}`
-// });
-
-// console.log("result", result);
-
-// setCode(result.code);
-
-// handleGetAccess();
-
-
-// }
-
-
-// //http data request
-
-
-// async function handleGetAccess() {  
-// const {  
-// data: { access\_token }  
-// } = await axios.get(`https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=${NV_APP_ID} &client_secret=${NV_APP_SECRET} &code=${code}&state=${STATE_STRING}`);
-
-
-// const config = {
-//   headers: {
-//     Authorization: `Bearer ${access_token}`
-//   }
-// };
-
-// setToken(data.access_token);
-
-// const { data } = await axios.get(
-//   "https://openapi.naver.com/v1/nid/me",
-//   config
-// );
-// console.log(data);
-// setUser(data);
-
-
-// }
-
-// //TEXT press -> handlePressAsync()  
-// return (
-
-// //네이버 아이디로 시작하기
-// //TEXT press -> handlePressAsync()
-//   return (
-//     <View style={styles.container}>
-//       <TouchableOpacity onPress={handlePressAsync}>
-//         <Text>네이버 아이디로 시작하기</Text>
-//       </TouchableOpacity>
-//     </View>
-//   );
-// }
