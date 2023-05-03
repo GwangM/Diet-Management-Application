4b63b6b8175329ac6b0cb6953bee3f678f005bd9
rhkdaud268@gmail.com <rhkdaud268@gmail.com>
2023-05-04 02:14:01 +0900

    eject complete

--- src/components/screens/FoodAnalysis.js
+++ src/components/screens/FoodAnalysis.js
@@ -12,3 +12,13 @@ function FoodAnalysis({navigation}) {
   );
 }
 export default FoodAnalysis;
+
+// » android: userInterfaceStyle: Install expo-system-ui in your project to enable this feature.
+// ✔ Config synced
+
+// ➡️  Next steps
+// › 🍫 When CocoaPods is installed, initialize the project workspace: npx pod-install
+// › 💡 You may want to run npx @react-native-community/cli doctor to help install any tools that your app may need to run your native projects.
+// › 🔑 Download your Android keystore (if you're not sure if you need to, just run the command and see): expo fetch:android:keystore
+// › 📁 The property assetBundlePatterns does not have the same effect in the bare workflow.
+//   Learn more: https://docs.expo.dev/bare/updating-your-app/#embedding-assets
