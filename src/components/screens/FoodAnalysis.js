031a61677381b51b5ae7af00634ec07f4c47d125
rhkdaud268@gmail.com <rhkdaud268@gmail.com>
2023-05-03 23:31:31 +0900

    before eject

---
+++ src/components/screens/FoodAnalysis.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet,View } from "react-native";
+
+function FoodAnalysis({navigation}) {
+  //사진 찍는 버튼, 갤러리에서 가져오는 버튼, 이미지 전송 버튼 적용
+  
+
+  return (
+    <View>
+      
+      </View>
+  );
+}
+export default FoodAnalysis;
